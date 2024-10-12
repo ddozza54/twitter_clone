5885bc71d0513fb60fab33d216b5883834540c46
ddozza54 <bananamayong@gmail.com>
2024-10-12 21:17:52 +0900

    ✨ FireBase init

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 import { useEffect, useState } from 'react'
 import LoadingScreen from './components/loading-screen'
+import { auth } from './firebase'
 
 const GlobalStyles = createGlobalStyle`
 ${reset};
@@ -20,7 +21,7 @@ body{
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const init = async () => {
-    //wait for firebase
+    await auth.authStateReady();
     setTimeout(() => setIsLoading(false), 2000);
   }
   useEffect(() => {
